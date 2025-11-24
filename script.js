0c8ec3ee7f36d9c8b8e33480bcc64c3530257629
Zaskia Jihan <kiahanabil@gmail.com>
2025-11-24 19:32:32 +0700

    Tambah js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const display = document.getElementById('display');
+    const historyList = document.getElementById('historyList');
+    const memoryValueDisplay = document.getElementById('memoryValue');
+    const buttons = document.querySelectorAll('.buttons button');
